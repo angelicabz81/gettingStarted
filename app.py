e951146fdf491bd25e44f9c7bae3c69e23709f59
Angelica <angelicabenitez@college.harvard.edu>
2025-11-18 22:57:21 -0500

    Update flask page with debugger

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask i love yu!!"
+    return "Hello, Flask i love boba!!"
+
+# in app.py
+# python3 -m flask --app app --debug run
+if __name__ == "__main__":
+    app.run(debug=True)
