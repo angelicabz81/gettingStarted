58039989db1033a7dae6b6052f371757d60675d7
Angelica <angelicabenitez@college.harvard.edu>
2025-11-19 00:09:08 -0500

    test sql in flask

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+
 app = Flask(__name__)
+
+# Path to SQLite database file
+DATABASE = "wardrobe.db"
+
+# Database connection management
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row # Enable dictionary-like row access
+    return g.db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 @app.route("/")
 def home():
